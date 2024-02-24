305563aab7cec8ef6a8511aaad4e13d9b64542ad
Anila Thomas <110098281+forger234@users.noreply.github.com>
2024-02-24 16:34:45 +0530

    Linear Search

---
+++ LinearSearch.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+void main()
+{
+    int a[100],n,s,i,flag=0;
+    printf("Enter the array size: ");
+    scanf("%d",&n);
+    printf("Enter the array elemnts: ");
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    printf("Enter the element for search: ");
+    scanf("%d",&s);
+    for(i=0;i<n;i++) 
+    { 
+    if(s==a[i])
+    {
+        flag=1;
+        break;
+    }}
+    if(flag==1){
+        printf("Element found %d..",s);
+    }
+        else{
+printf("Element not found  %d.",s);
+        }
+    }
